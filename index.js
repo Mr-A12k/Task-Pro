1c670eeb42f5639049302065a4a397f20cdef92e
Mr-A12k <kabil448446@gmail.com>
2024-08-26 15:12:59 +0530

    index changed'

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const assignmentRouter = require('./routes/assignmentRouter');
+const coursrDetailsRouter = require('./routes/courseDetailsRouter');
+const courseRouter = require('./routes/courseRouter');
+const loginRouter = require('./routes/loginRouter');
+const registerRouter = require('./routes/registerRouter');
+const progressRouter = require('./routes/progressRouter');
+const protectedRouter = require('./routes/protectedRouter');
+
 const app = express();
 
 app.use(express.json());
+app.use('/api',registerRouter);
+app.use('/api',loginRouter);
+app.use('/api/assignment',assignmentRouter);
+app.use('/api/course',courseRouter);
+app.use('/api/courseDetails',coursrDetailsRouter);
+app.use('/api/progress',progressRouter);
+app.use('/api/protected',protectedRouter);
+
+
 const port = 7000;
 
 app.get('/', (req, res) => {
@@ -17,7 +34,7 @@ app.post('/student',(req,res)=>{
 })
 
 app.get('/studentDetails', (req,res)=>{
-    //const doc
+   
 })
 
 
